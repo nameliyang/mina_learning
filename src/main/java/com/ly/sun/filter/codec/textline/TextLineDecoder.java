912ec187d6522899bb2c5bc79839f06e5e3be67e
liyang <1289714862@qq.com>
2017-06-15 10:23:09 +0800

    textlinedecoder

--- src/main/java/com/ly/sun/filter/codec/textline/TextLineDecoder.java
+++ src/main/java/com/ly/sun/filter/codec/textline/TextLineDecoder.java
@@ -1,20 +1,40 @@
 package com.ly.sun.filter.codec.textline;
 
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+
 import com.ly.sun.core.buffer.IoBuffer;
 import com.ly.sun.filter.codec.ProtocolDecoder;
 import com.ly.sun.filter.codec.ProtocolDecoderOutput;
 import com.ly.sun.transport.socket.nio.NioSocketSession;
 
 public class TextLineDecoder implements ProtocolDecoder{
-	
+    private final Charset charset;
+    
+    public TextLineDecoder(Charset charset){
+    	this.charset = charset;
+    }
+    
+    public TextLineDecoder() {
+    	this(Charset.defaultCharset());
+	}
+    
 	@Override
 	public void decode(NioSocketSession session, IoBuffer in, ProtocolDecoderOutput out) throws Exception {
-		
+		getContext(session);
 		
 	}
 	
 	
-	public void decodeAuto(Context context,NioSocketSession session,IoBuffer in,ProtocolDecoderOutput out){
+	private Context getContext(NioSocketSession session) {
+		
+		return null;
+	}
+
+	public void decodeAuto(Context context,NioSocketSession session,IoBuffer in,ProtocolDecoderOutput out) throws CharacterCodingException{
 		int oldPos = in.position();
 		int oldLimit = in.limit();
 		int matchCount = context.getMatchCount();
@@ -46,20 +66,35 @@ public class TextLineDecoder implements ProtocolDecoder{
 				ioBuffer.flip();
 				ioBuffer.limit(ioBuffer.limit() - matchCount);
 				
-				byte[] data = new byte[ioBuffer.limit()];
+				try{
+					byte[] data = new byte[ioBuffer.limit()];
+					ioBuffer.get(data);
+					CharsetDecoder decoder = context.getDecoder();
+					CharBuffer charBuffer = decoder.decode(ByteBuffer.wrap(data));
+				    String str = charBuffer.toString();
+	                writeText(session, str, out);
+				}finally{
+					ioBuffer.clear();
+				}
 				
 			}
 		}
 	}
 	
-	private class Context{
+	 private void writeText(NioSocketSession session, String text, ProtocolDecoderOutput out) {
+		 out.write(text);
+	 }
+
+	class Context{
 		
 		private final IoBuffer buf;
 		
+		private final CharsetDecoder decoder;
+		
 		public Context(int bufferLength){
 			buf = IoBuffer.allocate(bufferLength).setAutoExpand(true);
+		    decoder = charset.newDecoder();
 		}
-		
 		
 		public int getMatchCount() {
 			return 0;
@@ -71,6 +106,10 @@ public class TextLineDecoder implements ProtocolDecoder{
 		
 		public IoBuffer getIoBuffer(){
 			return buf;
+		}
+		
+		public CharsetDecoder getDecoder(){
+			return decoder;
 		}
 	}
 }	
