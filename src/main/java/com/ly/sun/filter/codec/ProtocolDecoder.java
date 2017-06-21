0df5ac31a28137225249aa7f60616bb32db10686
liyang <1289714862@qq.com>
2017-06-21 09:22:42 +0800

    SocketNioSession 改为Iosession

--- src/main/java/com/ly/sun/filter/codec/ProtocolDecoder.java
+++ src/main/java/com/ly/sun/filter/codec/ProtocolDecoder.java
@@ -1,11 +1,11 @@
 package com.ly.sun.filter.codec;
 
 import com.ly.sun.core.buffer.IoBuffer;
-import com.ly.sun.transport.socket.nio.NioSocketSession;
+import com.ly.sun.core.session.IoSession;
 
 public interface ProtocolDecoder {
 
-	void decode (NioSocketSession session,IoBuffer bin,ProtocolDecoderOutput out) throws Exception;
+	void decode (IoSession session,IoBuffer bin,ProtocolDecoderOutput out) throws Exception;
 	
 	
 	
