328a6b7e2b924a6695c1f2bcb7041a054d1328ad
liyang <1289714862@qq.com>
2017-06-27 17:34:25 +0800

    loggerTest

--- src/test/java/com/ly/sun/log/LogEnableTest.java
+++ src/test/java/com/ly/sun/log/LogEnableTest.java
@@ -1,8 +1,6 @@
 package com.ly.sun.log;
 
 
-import java.util.Date;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,19 +9,29 @@ public class LogEnableTest {
 	private static final Logger logger = LoggerFactory.getLogger(LogEnableTest.class);
 	
 	public static void main(String[] args) throws InterruptedException {
-		logger.error("{} {} {} {} {}",new Object[]{"hello",1,new Date(),1,
-			"sdfasdfsdddddddddddddddddddddddddddddddddddddd"});
-		
- 		for(;;){
-// 			logger.info("{} {} ",
-// 					"17/06/26 22:02:41 ERROR log.LogEnableTest: hello 1 Mon Jun 26 22:02:41 GMT+08:00 2017 1 sdfasdfsdddddddddddddddddddddddddddddddddddddd",
-// 					"17/06/26 22:02:41 ERROR log.LogEffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7 1 sdfasdfsdddddddddddddddddddddddddddddddddddddd");
- 			
-			logger.info(
- 					"17/06/26 22:02:41 ERROR log.LogEnableTest: hello 1 Mon Jun 26 22:02:41 GMT+08:00 2017 1 sdfasdfsdddddddddddddddddddddddddddddddddddddd"+
- 					"17/06/26 22:02:41 ERROR log.LogEffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7 1 sdfasdfsdddddddddddddddddddddddddddddddddddddd");
+		LoggerTestThread thread1 = new LoggerTestThread();
+		LoggerTestThread thread2 = new LoggerTestThread();
+		thread1.start();
+		thread2.start();
+	}
+	private static final String str1 = "17/06/26 22:02:41 ERROR log.LogEnableTest: hello 1 Mon Jun 26 22:02:41 GMT+08:00 2017 1 sdfasdfsdddddddddddddddddddddddddddddddddddddd";
+	private static final String str2 = "17/06/26 22:02:41 ERROR log.LogEffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7 1 sdfasdfsdddddddddddddddddddddddddddddddddddddd";
 
- 			Thread.sleep(10);
- 		}
+
+	private static class LoggerTestThread extends Thread{
+		@Override
+		public void run() {
+			for(;;){
+	 			logger.info("{} {} ",str1,str2);
+//				logger.info(
+//	 					"17/06/26 22:02:41 ERROR log.LogEnableTest: hello 1 Mon Jun 26 22:02:41 GMT+08:00 2017 1 sdfasdfsdddddddddddddddddddddddddddddddddddddd"+
+//	 					"17/06/26 22:02:41 ERROR log.LogEffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7 1 sdfasdfsdddddddddddddddddddddddddddddddddddddd");
+	 			try {
+					Thread.sleep(5);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+	 		}
+		}
 	}
 }
