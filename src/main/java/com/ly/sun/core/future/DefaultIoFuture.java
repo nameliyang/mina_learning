345eb7e430be9048a8104c0d4b56d7ebf6eb3527
liyang <1289714862@qq.com>
2017-06-27 18:51:33 +0800

    implement future await

--- src/main/java/com/ly/sun/core/future/DefaultIoFuture.java
+++ src/main/java/com/ly/sun/core/future/DefaultIoFuture.java
@@ -6,23 +6,48 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DefaultIoFuture implements IoFuture{
-
+	
 	private static final Logger logger = LoggerFactory.getLogger(DefaultIoFuture.class);
 	
-	private volatile boolean isDone = false;
+	private volatile boolean ready = false;
+	
+	private static final Object lock = new Object();
+	
+	private static final Long DEFAULT_TIME_OUT = 1000L;
+	
+	private Object value;
 	
 	@Override
 	public boolean isDone() {
-		return isDone;
+		return ready;
 	}
-
+	
+	public void setValue(Object value){
+		this.value = value;
+		ready = true;
+		synchronized (lock) {
+			lock.notifyAll();	
+		}
+	}
+	
+	
 	@Override
 	public IoFuture await() throws InterruptedException {
-		return null;
+		synchronized (lock) {
+			while(!ready){
+				lock.wait(DEFAULT_TIME_OUT);
+			}
+			return this;
+		}
 	}
 
 	@Override
 	public boolean await(long timeout) throws InterruptedException {
+		synchronized (lock) {
+			while(!ready){
+				
+			}
+		}
 		return false;
 	}
 
